75ba3bb7d4fc1642d06509a0f38e96126393371a
Damian Galeano <dj.damian.b@hotmail.com>
2023-03-05 00:28:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+my_string = "This is a String"
+print(my_string)
+
+my_age = 34
+print(my_age)
+
+x = True
+y = False
+print(x)
+print(y)
+
+some_fruits = ["pinaple", "orange", "banana"]
+print(some_fruits)
+
+
+first_three_words = my_string[0:3]
+print(first_three_words)
+
+take_element = some_fruits[0]
+print(take_element)
+
+add_then = my_age + 10
+print(add_then)
+
+last_element = some_fruits[-1]
+print(last_element)
+
+names = 'harry,alex,susie,jared,gail,conner'
+
+set_names = names.split(',')
+print(set_names)
